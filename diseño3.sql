22b4d5a43a4bca94d61e4e814ddac37823e8e981
KevinSaltos <KevnSaltos@hotmail.com>
2021-10-28 13:58:53 -0500

    final

--- diseño3.sql
+++ diseño3.sql
@@ -10,3 +10,11 @@ insert into duenios(correo, telf_duenio, num_licencia, nombre, lugar_trabajo) va
 /* Insercion masiva*/
 insert into duenio values(generate_series(3, 10),)
 
+
+
+
+alter table nombreTabla add column nombreRegistro tipoDato(valor) not null o nada;
+alter table clientes add column est_rec character(1)
+
+UPDATE nombre_tabla SET columna1 = 'nuevo_valor' WHERE NombrecampoPK = 'valor1';
+update clientes set est_rec = ‘i’ where id_cliente = 1;
